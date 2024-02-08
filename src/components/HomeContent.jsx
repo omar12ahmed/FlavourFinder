3c9aef7c5a6adad29e17bd0c8bd60efebbc47029
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-08 20:59:51 +0000

    added description and buttons to call API's on Homepage

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -6,14 +6,26 @@ function
 
     return (
         <wrapper>
-             <div className="p-5 mb-4 bg-body-tertiary rounded-3 ">
-      <div className="container-fluid py-5 jumboCopy baner">
-        <h1 className="display-5 fw-bold">Flavour Finder</h1>
-        <p className="col-md-12 fs-4">The tastiest recipes on the Internet</p>
-        
-      </div>
-    </div>
-    <button className="btn btn-primary btn-lg" type="button">Find your next dinner!</button>
+            {/* jumbotron */}
+            <div className="p-5 mb-4 bg-body-tertiary rounded-3 baner">
+                <div className="container-fluid py-5 jumboCopy jumbo-box">
+                    <h1 className="display-5 fw-bold">Flavour Finder</h1>
+                </div>
+            </div>
+            {/* description */}
+            <div className='text-box'>
+                <h2>The tasties recipies on the Internet!</h2>
+                <p>Explore a world of flavors with our diverse collection of recipes!
+                    Whether you're craving a quick weeknight dinner, a decadent dessert, or a refreshing beverage,
+                    our recipes have got you covered. Browse through our culinary creations and embark on a delicious
+                    journey in your own kitchen. From easy-to-follow instructions to mouthwatering photos,
+                    discover the joy of cooking with our enticing recipe selection. What's on your plate today?</p>
+                {/* button - conected to API */}
+                <div className='API-buttons'>
+                    <button className="btn btn-primary btn-lg" type="button">Find your next dinner!</button>
+                    <button className="btn btn-primary btn-lg" type="button">Find your next drink!</button>
+                </div>
+            </div>
         </wrapper>
     )
 }
