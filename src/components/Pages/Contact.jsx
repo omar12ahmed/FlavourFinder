1f36fc221902989b81cf2c94ca46c9b3bd7c0080
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2024-02-12 16:29:00 +0000

    added articles to contact page

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,11 +1,13 @@
 import ContactPage from "../ConatctPage"
 import Aboutus from "../Aboutus"
+import ArtilceApi from "../ArticleApi"
 
 function Contact(){
 
     return(
         <div>
             <Aboutus/>
+           <ArtilceApi/>
            <ContactPage/>
         </div>
     )
