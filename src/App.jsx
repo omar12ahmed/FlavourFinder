394b8993450cefbfe0879b0df14b521f9ba86485
Y-usman <uthmanyusuf1998@gmail.com>
2024-02-13 22:59:35 +0000

    dropped user authentication feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,8 @@ import Contact from './pages/Contact';
 import Drinks from './pages/Drinks';
 import Recipe from './pages/Recipe';
 import Footer from './components/Footer';
-import ProtectedRoute from './components/ProtectedRoute';
 import UserPage from './pages/UserPage';
-import LoginButton from './components/LoginButton';
-// import LogoutButton from './components/LogoutButton';
 import "../src/assets/css/food.css";
-
 
 
 import './App.css'
@@ -25,22 +21,13 @@ function App() {
       <Router>
         <div>
           <Header />
-          {/* <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/recipe" element={<Recipe />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/drinks" element={<Drinks />} />
-          </Routes> */}
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/recipe" element={<Recipe />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/drinks" element={<Drinks />} />
-              {/* <Route path="/UserPage" element={<ProtectedRoute component={UserPage} />} /> */}
               <Route path="/UserPage" element={<UserPage />} />
-           {/* <LoginButton />  */}
-           </Routes>
-        
+          </Routes>
         </div>
       </Router>
       <Footer />
