02292d2609c1ec37b5285b9e14fb3220b1ed4597
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-02-14 12:13:51 +0000

    added code for fav webpage - no styling yet

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -5,52 +5,14 @@ import * as bootstrap from 'bootstrap';
 function UserPage() {
     return (
         <>
-            <section className='form'>
-                <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
-                </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </section>
-            <section className='caroussel'>
-                <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src="../assets/images/images1" className="d-block w-100" alt="pasta"/>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="../assets/images/images5" className="d-block w-100" alt="Aperol"/>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="../assets/images/images7" className="d-block w-100" alt="Mexican food"/>
-                        </div>
-                        <div className="carousel-item active">
-                            <img src="../assets/images/images3" className="d-block w-100" alt="Pimm's Cup"/>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="../assets/images/images9" className="d-block w-100" alt="pancakes"/>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="../assets/images/images6" className="d-block w-100" alt="Bloody Mary"/>
-                        </div>
+            <section className='fav-container'>
+                <div className='fav-item'>
+                    <h3>name</h3>
+                    <img src=''></img>
+                    <div className='instruction'>
+                        <p>instruction</p>
+                        <button>remove</button>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
                 </div>
             </section>
         </>
