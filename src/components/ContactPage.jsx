fecf5b544cf254f3d0362056d3f29e0ffda7e302
Omar Ibrahim <omaribrahim@Omars-MacBook-Air-2.local>
2024-02-15 03:58:53 +0000

    fixed styling and removed TOB component

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -35,138 +35,141 @@ function ContactPage() {
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		if (
-			!formData.name ||
-			!formData.email ||
-			!formData.reasons ||
-			!formData.description
-		) {
-			setError(`Please fill in ${formData}fields before submitting the form.`);
-			return;
+		if (!formData.name) {
+			setError(`Please fill in name field before submitting the form.`);
+		} else if (!formData.email) {
+			setError(`Please fill in the email field before submitting the form.`);
+
+		} else if (!formData.reasons) {
+			setError(`Please fill in reason for contact field before submitting the form.`);
+
+		} else if (!formData.description) {
+			setError(`Please fill in description field before submitting the form.`);
+
 		} else {
 			setError("");
-
-			window.prompt("Form submitted, thank you!");
+			window.alert("Form submitted, thank you!")
 		}
 	};
 
+
 	return (
-        <div className="contactRow">
-		<div
-			className="container contactPage"
-		>
-			<h1 id="contact" style={{ marginBottom: "20px" }}>
-				Contact us{" "}
-			</h1>
-
-			{error && <div style={{ color: "red" }}>{error}</div>}
-
-			<form
-				style={{ display: "block", textAlign: "center" }}
-				onSubmit={handleSubmit}
+		<div className="contactRow">
+			<div
+				className="container contactPage"
 			>
-				<label style={{ display: "block", marginBottom: "10px" }}>
-					Name:
-					<br />
-					<input
-						type="text"
-						name="name"
-						value={formData.name}
-						onChange={handleChange}
-						style={{
-							width: "100%",
-							padding: "8px",
-							boxSizing: "border-box",
-							marginBottom: "10px",
-						}}
-					/>
-				</label>
-				<br />
-				
-				<label style={{ display: "block", marginBottom: "10px" }}>
-					Email:
-					<br />
-					<input
-						type="text"
-						name="email"
-						value={formData.email}
-						onChange={handleChange}
-						style={{
-							width: "100%",
-							padding: "8px",
-							boxSizing: "border-box",
-							marginBottom: "10px",
-						}}
-					/>
-				</label>
-				<br />
+				<h1 id="contact" style={{ marginBottom: "20px" }}>
+					Contact us{" "}
+				</h1>
 
-				<label style={{ display: "block", marginBottom: "10px" }}>
-					Reason for contact:
-				</label>				
-				<select
-					name="reasons"
-					value={formData.reasons}
-					onChange={handleChange}
-					style={{
-						width: "100%",
-						padding: "8px",
-						boxSizing: "border-box",
-						marginBottom: "30px",
-					}}
+				{error && <div style={{ color: "red" }}>{error}</div>}
+
+				<form
+					style={{ display: "block", textAlign: "center" }}
+					onSubmit={handleSubmit}
 				>
-					<option>Select..</option>
-					{options.map((option) => (
-						<option key={option.value} value={option.value}>
-							{option.label}
-						</option>
-					))}
-				</select>
-				<br />
+					<label style={{ display: "block", marginBottom: "10px" }}>
+						Name:
+						<br />
+						<input
+							type="text"
+							name="name"
+							value={formData.name}
+							onChange={handleChange}
+							style={{
+								width: "100%",
+								padding: "8px",
+								boxSizing: "border-box",
+								marginBottom: "10px",
+							}}
+						/>
+					</label>
+					<br />
 
-				<label style={{ display: "block", marginBottom: "10px" }}>
-					Describe the problem with as much detail as possible
-					<textarea
-						name="description"
-						cols="40"
-						rows="10"
-						value={formData.description}
+					<label style={{ display: "block", marginBottom: "10px" }}>
+						Email:
+						<br />
+						<input
+							type="text"
+							name="email"
+							value={formData.email}
+							onChange={handleChange}
+							style={{
+								width: "100%",
+								padding: "8px",
+								boxSizing: "border-box",
+								marginBottom: "10px",
+							}}
+						/>
+					</label>
+					<br />
+
+					<label style={{ display: "block", marginBottom: "10px" }}>
+						Reason for contact:
+					</label>
+					<select
+						name="reasons"
+						value={formData.reasons}
 						onChange={handleChange}
 						style={{
 							width: "100%",
 							padding: "8px",
 							boxSizing: "border-box",
-							marginBottom: "10px",
+							marginBottom: "30px",
 						}}
-					></textarea>
-				</label>
-				<label style={{ display: "block", marginBottom: "10px" }}>
-					Attachments:
+					>
+						<option>Select..</option>
+						{options.map((option) => (
+							<option key={option.value} value={option.value}>
+								{option.label}
+							</option>
+						))}
+					</select>
+					<br />
+
+					<label style={{ display: "block", marginBottom: "10px" }}>
+						Describe the problem with as much detail as possible
+						<textarea
+							name="description"
+							cols="40"
+							rows="10"
+							value={formData.description}
+							onChange={handleChange}
+							style={{
+								width: "100%",
+								padding: "8px",
+								boxSizing: "border-box",
+								marginBottom: "10px",
+							}}
+						></textarea>
+					</label>
+					<label style={{ display: "block", marginBottom: "10px" }}>
+						Attachments:
+						<input
+							type="file"
+							name="attachments"
+							onChange={handleChange}
+							style={{
+								width: "100%",
+								padding: "8px",
+								boxSizing: "border-box",
+								marginBottom: "10px",
+							}}
+						/>
+					</label>
+					<br />
 					<input
-						type="file"
-						name="attachments"
-						onChange={handleChange}
+						type="submit"
+						onChange={handleSubmit}
 						style={{
-							width: "100%",
-							padding: "8px",
-							boxSizing: "border-box",
-							marginBottom: "10px",
+							background: "rgb(3 246 230 / 38%)",
+							fontSize: "20px",
+							borderRadius: "10px",
+							padding: "10px",
 						}}
 					/>
-				</label>
-				<br />
-				<input
-					type="submit"
-					onChange={handleSubmit}
-					style={{
-						background: "rgb(3 246 230 / 38%)",
-						fontSize: "20px",
-						borderRadius: "10px",
-						padding: "10px",
-					}}
-				/>
-			</form>
-		</div></div>
+				</form>
+			</div></div>
 	);
 }
 export default ContactPage;
